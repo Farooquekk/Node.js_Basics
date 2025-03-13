168d2d28073ab25cbf62ff272e8c63e5d5d5854f
Farooque Sajjad <22sw040@students.muet.edu.pk>
2025-03-13 09:27:57 -0700

    Add files via upload

---
+++ prerequisite_03.js
@@ -0,0 +1,26 @@
+
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// // finding the length of the array
+// console.log(arr.length);
+
+// // functions are objects in JavaScript
+// function abcd(){
+
+// }
+// // finding the length of the function
+// console.log(abcd.length);
+// console.log(typeof abcd);
+// console.dir(abcd);
+
+// 
+
+function abcd(){
+    return 12;
+}
+
+
+
+
+
+var ans = abcd();
+console.log(ans);
